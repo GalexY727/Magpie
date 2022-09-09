6014a189d240c2bb24ac63571570bbe4f0dc3eee
Alexander Hamilton <alexander.hamilton@mrjaffesclass.com>
2022-09-09 12:45:04 -0700

    Added teacher response

--- activity2/Magpie2.java
+++ activity2/Magpie2.java
@@ -6,7 +6,7 @@ package activity2;
  * <ul><li>
  *       Uses indexOf to find strings
  * </li><li>
- * 		    Handles responding to simple words and phrases 
+ * 		    Handles responding to simple words and phrases
  * </li></ul>
  * This version uses a nested if to handle default responses.
  * @author Laurie White
@@ -15,17 +15,17 @@ package activity2;
 public class Magpie2
 {
 	/**
-	 * Get a default greeting 	
+	 * Get a default greeting
 	 * @return a greeting
 	 */
 	public String getGreeting()
 	{
 		return "Hello, let's talk.";
 	}
-	
+
 	/**
 	 * Gives a response to a user statement
-	 * 
+	 *
 	 * @param statement
 	 *            the user statement
 	 * @return a response based on the rules given
@@ -33,16 +33,25 @@ public class Magpie2
 	public String getResponse(String statement)
 	{
 		String response = "";
-		if (statement.indexOf("no") >= 0)
+		if (statement.contains("no"))
 		{
 			response = "Why so negative?";
 		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
+		else if (statement.contains("mother")
+				|| statement.contains("father")
+				|| statement.contains("sister")
+				|| statement.contains("brother"))
 		{
 			response = "Tell me more about your family.";
+		}
+		else if (statement.contains("Mr."))
+		{
+			response = "Mr. Jaffe is a the best teacher, I agree.";
+		}
+		else if (statement.contains("dog")
+				|| statement.contains("cat"))
+		{
+			response = "Tell me more about your pets.";
 		}
 		else
 		{
