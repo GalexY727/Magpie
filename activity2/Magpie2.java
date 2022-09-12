a2719a38c8faceefab8c2f4bb63eacd6fd7d30a0
Alexander Hamilton <alexander.hamilton@mrjaffesclass.com>
2022-09-12 12:48:05 -0700

    added a bunch of fun things-- thanks copilot

--- activity2/Magpie2.java
+++ activity2/Magpie2.java
@@ -53,6 +53,168 @@ public class Magpie2
 		{
 			response = "Tell me more about your pets.";
 		}
+		//trim to check if anything was said
+		else if (statement.trim().length() == 0)
+		{
+			response = "Say something, please.";
+		}
+		else if (statement.contains("school"))
+		{
+			response = "School is fun.";
+		}
+		else if (statement.contains("food"))
+		{
+			response = "I like food.";
+		}
+		else if (statement.contains("weather"))
+		{
+			response = "The weather is nice.";
+		}
+		else if (statement.contains("sports"))
+		{
+			response = "I like sports.";
+		}
+		else if (statement.contains("music"))
+		{
+			response = "I love music, have you listened to Data Loss by JKuch?";
+		}
+		//import spotify cli and get all songs from a user's liked library
+		else if (statement.contains("spotify"))
+		{
+			response = "I like spotify.";
+		}
+		else if (statement.contains("computer"))
+		{
+			response = "I like computers.";
+		}
+		else if (statement.contains("game"))
+		{
+			response = "I like games.";
+		}
+		else if (statement.contains("movie"))
+		{
+			response = "I like movies.";
+		}
+		else if (statement.contains("tv"))
+		{
+			response = "I like tv.";
+		}
+		else if (statement.contains("book"))
+		{
+			response = "I like books.";
+		}
+		else if (statement.contains("art"))
+		{
+			response = "I like art.";
+		}
+		else if (statement.contains("science"))
+		{
+			response = "I like science.";
+		}
+		else if (statement.contains("math"))
+		{
+			response = "I like math.";
+		}
+		else if (statement.contains("history"))
+		{
+			response = "I like history.";
+		}
+		else if (statement.contains("english"))
+		{
+			response = "I like english.";
+		}
+		else if (statement.contains("programming"))
+		{
+			response = "I like programming.";
+		}
+		else if (statement.contains("coding"))
+		{
+			response = "I like coding.";
+		}
+		else if (statement.contains("python"))
+		{
+			response = "I like python.";
+		}
+		else if (statement.contains("java"))
+		{
+			response = "I like java.";
+		}
+		else if (statement.contains("c++"))
+		{
+			response = "I like c++.";
+		}
+		else if (statement.contains("c"))
+		{
+			response = "I like c.";
+		}
+		else if (statement.contains("javascript"))
+		{
+			response = "I like javascript.";
+		}
+		else if (statement.contains("html"))
+		{
+			response = "I like html.";
+		}
+		else if (statement.contains("css"))
+		{
+			response = "I like css.";
+		}
+		else if (statement.contains("sql"))
+		{
+			response = "I like sql.";
+		}
+		else if (statement.contains("kotlin"))
+		{
+			response = "I like kotlin.";
+		}
+		else if (statement.contains("video games"))
+		{
+			response = "I like video games.";
+		}
+		else if (statement.contains("movies"))
+		{
+			response = "I like movies.";
+		}
+		else if (statement.contains("television"))
+		{
+			response = "I like television.";
+		}
+		else if (statement.contains("books"))
+		{
+			response = "I like books.";
+		}
+		else if (statement.contains("hobbies"))
+		{
+			response = "I like building robots, and ice skating with my friends.";
+		}
+		else if (statement.contains("friends"))
+		{
+			response = "I like friends... to bad they are all digital";
+		}
+		else if (statement.contains("family"))
+		{
+			response = "I dont have much of a family... but thats okay.";
+		}
+		else if (statement.contains("hate"))
+		{
+			response = "Why do you hate that?";
+		}
+		else if (statement.contains("like"))
+		{
+			response = "Why do you like that?";
+		}
+		else if (statement.contains("love"))
+		{
+			response = "Why do you love that?";
+		}
+		else if (statement.contains("dont like") || statement.contains("dislike"))
+		{
+			response = "Why do you dislike that?";
+		}
+		else if (statement.contains("I want"))
+		{
+			response = "What would it mean to you if you got that?";
+		}
 		else
 		{
 			response = getRandomResponse();
